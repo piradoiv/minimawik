8104b26664988a0d2758cac00ac6f203662aa93e
Ricardo Cruz <piradoiv@gmail.com>
2013-05-03 12:09:37 +0100

    Added REST verbs to the router

--- application/controllers/router.php
+++ application/controllers/router.php
@@ -2,9 +2,12 @@
 
 class Router extends CI_Controller {
 
+    private $method = 'GET';
+
     public function __construct()
     {
         parent::__construct();
+        $this->method = $_SERVER['REQUEST_METHOD'];
     }
 
     public function index()
@@ -12,9 +15,21 @@ class Router extends CI_Controller {
         $data['page_title'] = "Title";
         $data['content']    = uri_string();
 
-        $this->load->view('header', $data);
-        $this->load->view('content', $data);
-        $this->load->view('footer', $data);
+        // Show the page
+        if($this->method === 'GET') {
+            $this->load->view('header', $data);
+            $this->load->view('content', $data);
+            $this->load->view('footer', $data);
+        
+        // Create a page
+        } elseif ($this->method === 'PUT') {
+
+        // Update a page
+        } elseif ($this->method === 'POST') {
+            
+        }
+
+
     }
 
 }
